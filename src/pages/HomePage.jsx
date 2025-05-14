5b8ebd584890d34d3687ea7f0cd28d1b68af93c2
Nataliia <nkopichinckaya@gmail.com>
2025-05-15 01:05:13 +0200

    add HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import Button from '../components/baseUI/Button/Button'
+import s from './Page.module.css'
 
 function HomePage() {
-  return <div>HomePage</div>
+  return (
+    <div className={s.containerHome}>
+      <div className={s.titleContainer}>
+        <h1 className={s.h1Title}>Find your perfect rental car</h1>
+        <h3 className={s.pTitle}>
+          Reliable and budget-friendly rentals for any journey
+        </h3>
+        <Button>View Catalog</Button>
+      </div>
+    </div>
+  )
 }
 
 export default HomePage
